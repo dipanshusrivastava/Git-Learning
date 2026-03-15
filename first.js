cc91a95380c18c66d91a46edb5b844658ecc5237
Dipanshu Srivastava <dsrivastav547@gmail.com>
2026-03-15 16:41:29 +0530

    Final commit

--- first.js
+++ first.js
@@ -1,11 +1,18 @@
 const twoSum = (a, b) => {
     return a + b;
 }
-
+//two sum
 console.log(twoSum);
 
 const threeSum = (a, b, c) => {
     return a + b + c;
 }
-
+//threesum
 console.log(threeSum);
+
+
+const fourSum = (a, b, c, d) => {
+    return a + b + c + d;
+}
+//four sum
+console.log(fourSum);
